453a0d92275eb4649594143629c1cbe108f6d256
abraaonma <abraaonmazevedo@gmail.com>
2020-06-19 11:06:18 -0300

    Atualização de dados da turma

--- libphp/dadosdaturma.php
+++ libphp/dadosdaturma.php
@@ -1,5 +1,7 @@
-
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">Dados da Turma</h1>
+</div>
 <div class="form-group">
   <h4>Mensagen de boas vindas</h4>
   <p><i>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</i></p>
